4a1fb045f78e1996b96af44cdec0fc93ef3b1f8d
Tara Tohidi <tara.tohidi@gmail.com>
2022-08-22 14:03:09 -0700

    Challange #1523 & #1491

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+// 1523. Count Odd Numbers in an Interval Range
+let arr;
+let a;
+const countOdds = function (low, high) {
+  arr = [low];
+  a = low;
+  while (a < high) {
+    a += 1;
+    arr.push(a);
+  }
+  const oddArr = arr.filter((num) => num % 2 === 1);
+  return oddArr.length;
+};
+console.log(countOdds(1, 21));
+
+// 1491. Average Salary Excluding the Minimum and Maximum Salary
+
+const average = function (salary) {
+  const min = Math.min(...salary);
+  const max = Math.max(...salary);
+  const avgArr = salary.filter((num) => num !== min && num !== max);
+
+  const avgSum = avgArr.reduce((acc, cur) => acc + cur, 0);
+  return avgSum / avgArr.length;
+};
+
+console.log(average([1000, 2000, 3000]));
